ac000610bdc14adaeeb334d6f575bef82ce98190
Roberto Acuña <robertoacunapolo@gmail.com>
2021-07-01 07:30:59 -0500

    refactor: :truck: Added a new folder structure for the project

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,13 +11,13 @@ export default function Home() {
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
         <h1 className="text-6xl font-bold">
           Welcome to{" "}
-          <a className="text-blue-600" href="https://nextjs.org">
+          <a className="text-blue-500" href="https://nextjs.org">
             _facebook!
           </a>
         </h1>
 
         <p className="mt-3 text-2xl">
-          Get started by editing{" "}
+          This will be the new facebook redefined
           <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
             pages/index.js
           </code>
