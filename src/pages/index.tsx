6664b8be5dbbba992ce37e175f428633221a3748
Roberto Acuña <robertoacunapolo@gmail.com>
2021-07-17 17:13:01 -0500

    feat(Login): :sparkles: Completed the Login page UI

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
+import CapAmerica from "../assets/captainAmerica.jpeg";
 import FacebookTextLogo from "../assets/facebook_logo.svg";
 
 export default function Home() {
@@ -12,7 +13,7 @@ export default function Home() {
       <main className="h-screen bg-offWhite">
         <nav className="pt-4 ">
           {/* NavigationBar */}
-          <div className="max-w-7xl px-4 mx-auto xl:max-w-7xl">
+          <div className="max-w-7xl px-10 mx-auto xl:max-w-7xl">
             <div className="flex justify-between items-center">
               {/* Primary Navigation */}
               <div className="flex items-center ">
@@ -78,33 +79,144 @@ export default function Home() {
             </div>
           </div>
         </nav>
-        <div className="px-7 pt-20 mx-auto xl:max-w-7xl">
-          <div className="flex justify-between ">
+        <div className="px-14 pt-12 mx-auto xl:max-w-7xl">
+          <div className="flex justify-between  ">
             {/* Welcome Header {Component} */}
-            <section className="max-w-lg">
+            <div className="max-w-lg">
               <h1 className="text-4xl leading-x36 font-semibold">
-                Facebook te ayuda a{" "}
-                <span className="text-blue-500">comunicarte y compartir</span>{" "}
-                con las personas que forman parte de tu vida.
+                Facebook helps you <br />
+                <span className="text-blue-500">connect and share</span> with
+                the people who are part of your life.
               </h1>
 
-              <p className="text-gray-500 pt-3 text-lg">
-                ¿No tienes una cuenta todavía?{" "}
-                <span className="text-blue-500 font-semibold cursor-pointer">
-                  Crea una cuenta aquí
-                </span>{" "}
-                y comparte los mejores momentos
-              </p>
-            </section>
+              {/* Recent Login */}
+              <div className="pt-4 ">
+                <span className="text-sm text-gray-500">
+                  Click your picture or add your recent account
+                </span>
+                <div className="pt-2">
+                  <ul className="flex m-0">
+                    {/* RecentLogin (Value) */}
+                    <li className="recent-login-card mr-2 border-solid group">
+                      {/* Badge */}
+                      <span className="text-white -m-2 absolute right-0 top-0 z-10 rounded-full w-6 h-6 v-center bg-accent text-sm">
+                        4
+                      </span>
+                      <button className="absolute z-10  transition-colors group-hover:shadow-lg group-hover:bg-white left-0 top-0 m-2 w-6 h-6   rounded-full bg-blackTransparent">
+                        <div className="w-full v-center group-hover:text-black text-white transition-colors ">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="h-4 w-4"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth={2}
+                              d="M6 18L18 6M6 6l12 12"
+                            />
+                          </svg>
+                        </div>
+                      </button>
+
+                      <Image
+                        src={CapAmerica}
+                        className="object-cover h-48 w-full rounded-lg"
+                        alt="Capitan América"
+                      />
+
+                      <div className="v-center">
+                        <span className=" text-lg p-2 font-semibold cursor-pointer">
+                          Steve Rogers
+                        </span>
+                      </div>
+                    </li>
+                    {/* RecentLogin (No Value) */}
+                    <li className="recent-login-card">
+                      <div className="bg-gray-100 v-center">
+                        <div className="text-blue-500">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="h-5 w-5"
+                            viewBox="0 0 20 20"
+                            fill="currentColor"
+                          >
+                            <path
+                              fillRule="evenodd"
+                              d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v2H7a1 1 0 100 2h2v2a1 1 0 102 0v-2h2a1 1 0 100-2h-2V7z"
+                              clipRule="evenodd"
+                            />
+                          </svg>
+                        </div>
+                      </div>
+                      <div className="v-center">
+                        <button className="bg-none text-blue-500 hover:underline">
+                          Add Account
+                        </button>
+                      </div>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
 
             {/* Card of login */}
-            <section className="max-w-md w-full h-x443 mx-auto bg-white shadow-md rounded-md px-7 py-10 my-6">
-              <h1 className="text-xl font-semibold pb-6">
-                Que tal si le envías a alguien un mensaje deseandole un feliz
-                día?
-              </h1>
-              <div></div>
-            </section>
+            <div className="flex flex-col">
+              <section className="max-w-md divide-y-2 w-full  bg-white shadow-xl rounded-md">
+                {/* Card Login Header And Content */}
+                <div className="pt-4 pb-2 px-7">
+                  <h1 className="text-xl font-semibold pb-6">
+                    How about sending someone a message wishing them a happy
+                    day?
+                  </h1>
+                  {/* Form */}
+                  <form className="pb-6">
+                    <div className="w-full pb-3">
+                      <input
+                        type="text"
+                        className="h-12 w-full shadow-inner  rounded-lg bg-gray-100 leading-tight focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 p-4"
+                        placeholder="Email address"
+                      />
+                    </div>
+                    <div className="w-full">
+                      <input
+                        type="password"
+                        className="h-12 w-full shadow-inner  rounded-lg bg-gray-100 leading-tight focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 p-4"
+                        placeholder="Password"
+                      />
+                    </div>
+                  </form>
+                  <div className="pb-4 w-full">
+                    {/* Button Component */}
+                    <button className="bg-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-600 hover:bg-blue-600 transition-transform active:transform active:scale-95 font-semibold text-lg text-white w-full p-3 rounded-lg">
+                      Log In
+                    </button>
+                  </div>
+                  <div className="w-full flex justify-center">
+                    <span className="text-blue-500 font-semibold text-sm cursor-pointer hover:text-blue-600 hover:underline">
+                      Forgot your password
+                    </span>
+                  </div>
+                </div>
+                <div className="p-6 flex justify-center items-center text-center">
+                  <button className="bg-secondary-500 focus:outline-none focus:ring-2 focus:ring-green-500 hover:bg-secondary-600 transition-transform active:transform active:scale-95 font-semibold text-lg text-white  p-3 rounded-lg">
+                    Create New Account
+                  </button>
+                </div>
+              </section>
+              {/* Create Page For Bussiness Component */}
+              <span className="text-sm  max-w-md text-center pt-8 text-gray-800">
+                <button
+                  type="button"
+                  className="bg-none hover:underline font-semibold "
+                >
+                  Crea una página
+                </button>{" "}
+                para un personaje público, un grupo de música o un negocio
+              </span>
+            </div>
           </div>
         </div>
       </main>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,25 @@ module.exports = {
     "./src/pages/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: "media", // or 'media' or 'class'
   theme: {
     extend: {
       backgroundColor: {
         offWhite: "#FCFCFC",
+        blackTransparent: "rgba(0,0,0,25%)",
+        secondary: {
+          50: "#f6fbf4",
+          100: "#ecf8ea",
+          200: "#d0edca",
+          300: "#b3e2aa",
+          400: "#7bcd6a",
+          500: "#42b72a",
+          600: "#3ba526",
+          700: "#328920",
+          800: "#286e19",
+          900: "#205a15",
+        },
+        accent: "#FA3E3E",
       },
       spacing: {
         31.5: "7.5rem",
