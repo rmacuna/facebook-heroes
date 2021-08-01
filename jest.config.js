27090dc903e7692c22767953e85c9abde86e6030
Roberto Acuña <robertoacunapolo@gmail.com>
2021-08-01 13:12:02 -0500

    test:  :white_check_mark: Removed incorrect test for password-recover screen

--- jest.config.js
+++ jest.config.js
@@ -3,12 +3,14 @@ module.exports = {
     "**/*.{js,jsx,ts,tsx}",
     "!**/*.d.ts",
     "!**/node_modules/**",
+    "src/**/*.{js,jsx,ts,tsx}",
   ],
+  testEnvironment: "jsdom",
+  moduleDirectories: ["node_modules", "src"],
   setupFilesAfterEnv: ["<rootDir>/setupTests.js"],
   testPathIgnorePatterns: ["/node_modules/", "/.next/"],
   transform: {
     "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
-    "^.+\\.css$": "<rootDir>/config/jest/cssTransform.js",
     ".+\\.(css|styl|less|sass|scss|png|jpg|jpeg|ttf|woff|woff2)$":
       "jest-transform-stub",
   },
--- src/components/common/input/FCInput.spec.tsx
+++ src/components/common/input/FCInput.spec.tsx
@@ -17,6 +17,6 @@ it("Should render FCInput", () => {
 it("Should render FCInput with sm properties", () => {
   const { getByRole } = render(<FCInput inputSize="sm" />);
   const baseTextInput = getByRole("textbox");
-  expect(baseTextInput).toHaveClass("h-12");
+  expect(baseTextInput).toHaveClass("h-8");
   expect(baseTextInput).toBeInTheDocument();
 });
