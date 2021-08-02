e8d798af9bbada72595013cdd8776a72fc42a980
Roberto Acuña <robertoacunapolo@gmail.com>
2021-08-01 22:08:43 -0500

    refactor: updated navigation item test file

--- src/components/layout/navbar/__tests__/NavigationItem.spec.tsx
+++ src/components/layout/navbar/__tests__/NavigationItem.spec.tsx
@@ -8,7 +8,12 @@ import { screen, render } from "@testing-library/react";
 import NavigationItem from "../NavigationItem";
 
 it("Should render the navigation item with value Facebook Lite", () => {
-  render(<NavigationItem value="Facebook Lite" />);
+  render(
+    <NavigationItem
+      path="https://www.facebook.com/lite"
+      value="Facebook Lite"
+    />
+  );
   const element = screen.getByRole("listitem");
   expect(element).toHaveTextContent("Facebook Lite");
 });
